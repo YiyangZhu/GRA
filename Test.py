40ae8a543830fdf84c8ad1b679a21c353baa214b
Yiyang Zhu <ererandkeke@gmail.com>
2018-09-08 20:54:24 -0400

    test

--- Test.py
+++ Test.py
@@ -1,7 +1,7 @@
 import urllib
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
-
+import time
 
 file = open("CompanyLinks.txt", "r")
 f = open("CompanyNameRatings.txt","w")
@@ -15,22 +15,42 @@ while not theEnd:
         quote_page = Request(line, headers={'User-Agent': 'Mozilla/5.0'})
         page = urlopen(quote_page)
         soup = BeautifulSoup(page.read(),'html.parser')
-        #print(soup.prettify())
         
         #<p class="h1 strong tightAll" title="" data-company="ABIOMED"> ABIOMED </p>
+        #get company name
         name_box = soup.find('p',attrs={"class","h1 strong tightAll"})
-        #print("name box:",name_box)
         name = name_box.text.strip()
         print("company name is:",name)
         f.write("company name is:"+str(name)+"\n")
 
         #<div class="ratingNum">4.0</div>
+        #get company overall rating
         rating_box = soup.find('div', attrs={"class", "ratingNum"})
-        #print("rating box:",rating_box)
         rate = rating_box.text.strip()
         print("Company rating is:",rate)
-        f.write("Company rating is:"+str(rate)+"\n\n")
-        print()
+        f.write("Company rating is:"+str(rate)+"\n")
+        
+        #get company id
+        indexOfReviews = line.index("Reviews-E")
+        indexOfHtm = line.index(".htm")
+        id = line[indexOfReviews + 9:indexOfHtm]
+        print("Company id is:",id)
+        f.write("Company id is:"+str(id)+"\n")
+        
+        #get company subratings
+        beginPart = "https://www.glassdoor.com/api/employer/"
+        endPart = "-rating.htm?locationStr=&jobTitleStr=&filterCurrentEmployee=false&filterEmploymentStatus=REGULAR&filterEmploymentStatus=PART_TIME"
+        subratingPage = beginPart + id + endPart
+        quote_page2 = Request(subratingPage, headers={'User-Agent': 'Mozilla/5.0'})
+        page2 = urlopen(quote_page2)
+        soup2 = BeautifulSoup(page2.read(),'html.parser')
+        print("Company subrating page is: "+subratingPage+"\n")
+        f.write("Company subrating page is: "+subratingPage+"\n")
+        print(soup2.prettify()+"\n")
+        f.write(soup2.prettify()+"\n\n")
+        
+        #let Python rest for 10 seconds
+        time.sleep(10)
     else:
         theEnd = True
         
